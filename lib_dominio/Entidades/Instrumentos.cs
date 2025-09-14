73c5f5d23fd63cad2547a388f57f3bae0470fe86
JuanCasZa <juanjose-castro2@hotmail.com>
2025-09-14 13:59:46 -0500

    Clase membresia

--- lib_dominio/Entidades/Instrumentos.cs
+++ lib_dominio/Entidades/Instrumentos.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lib_dominio.Entidades
 {
-    internal class Instrumentos
+    public class Instrumentos
     {
+        [Key]public int Id { get; set; }
+        public string? NombreInstrumento { get; set; }
+        public int? CantidadEquip { get; set; }
+        public int? Piezas { get; set; }
+        public string? Marca { get; set; }
+        public string DescripcionGeneral { get; set; }
+        public bool? Estado { get; set; }
+        public int Proveedor { get; set; }
+        [ForeignKey("Proveedor")] public Proveedores? _Proveedor { get; set; }
     }
 }
---
+++ lib_dominio/Entidades/Membresias.cs
@@ -0,0 +1,9 @@
+﻿namespace lib_dominio.Entidades
+{
+    public class Membresias
+    {
+        public int Id { get; set; }
+        public decimal? Valor { get; set; }
+        public string? TipoMembresia { get; set; }
+    }
+}
