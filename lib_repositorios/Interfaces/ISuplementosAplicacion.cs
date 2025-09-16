42ffaf55639f0ae5a31f29ca9b904d9ba5f66515
Julian Villada <juliandavillada1606@gmail.com>
2025-09-15 22:08:44 -0500

    Suplementos prueba unitaria

---
+++ lib_repositorios/Interfaces/ISuplementosAplicacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_dominio.Entidades;
+
+namespace lib_repositorios.Interfaces
+{
+    public interface ISuplementosAplicacion
+    {
+        void Configurar(string StringConexion);
+        List<Suplementos> Listar();
+        Suplementos? Guardar(Suplementos? entidad);
+        Suplementos? Modificar(Suplementos? entidad);
+        Suplementos? Borrar(Suplementos? entidad);
+    }
+}
--- ut_presentacion/Repositorios/SuplementosPrueba.cs
+++ ut_presentacion/Repositorios/SuplementosPrueba.cs
@@ -3,10 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using ut_presentacion.Nucleo;
 
 namespace ut_presentacion.Repositorios
 {
-    internal class SuplementosPrueba
+    [TestClass]
+    public class SuplementosPrueba
     {
+        private readonly IConexion? iConexion;
+        private Suplementos? entidad;
+        private Proveedores? proveedor;
+
+        public SuplementosPrueba()
+        {
+            iConexion = new Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+        }
+
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            Assert.AreEqual(true, Borrar());
+        }
+
+        public bool Listar()
+        {
+            var lista = iConexion!.Suplementos!.Include(f => f._Proveedor).ToList();
+            return lista.Count > 0;
+        }
+
+        public bool Guardar()
+        {
+            this.proveedor = EntidadesNucleo.Proveedores();
+            iConexion!.Proveedores!.Add(proveedor);
+            iConexion.SaveChanges();
+
+            this.entidad = EntidadesNucleo.Suplementos();
+            this.entidad.Proveedor = proveedor.Id;
+
+            iConexion.Suplementos!.Add(entidad);
+            iConexion.SaveChanges();
+
+            return true;
+        }
+
+        public bool Modificar()
+        {
+            this.entidad!.Valor = 155000m;
+            var entry = this.iConexion!.Entry<Suplementos>(this.entidad);
+            entry.State = EntityState.Modified;
+            this.iConexion!.SaveChanges();
+            return true;
+        }
+        public bool Borrar()
+        {
+            this.iConexion!.Suplementos!.Remove(this.entidad!);
+            this.iConexion!.SaveChanges();
+
+            return true;
+        }
     }
 }
