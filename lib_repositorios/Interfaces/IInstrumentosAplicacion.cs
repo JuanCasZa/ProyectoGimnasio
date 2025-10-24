107af2c2a61d24834d5962ca6bdd30b0c8e3d554
STEFANY ARIAS GARCES <stefanyarias1121887@correo.itm.edu.co>
2025-10-24 15:08:49 -0500

    Adicion de controllers de entidades

--- lib_repositorios/Interfaces/IInstrumentosAplicacion.cs
+++ lib_repositorios/Interfaces/IInstrumentosAplicacion.cs
@@ -10,6 +10,7 @@ namespace lib_repositorios.Interfaces
     public interface IInstrumentosAplicacion
     {
         void Configurar(string StringConexion);
+        List<Instrumentos> PorNombreInstrumento(Instrumentos? entidad);
         List<Instrumentos> Listar();
         Instrumentos? Guardar(Instrumentos? entidad);
         Instrumentos? Modificar(Instrumentos? entidad);
