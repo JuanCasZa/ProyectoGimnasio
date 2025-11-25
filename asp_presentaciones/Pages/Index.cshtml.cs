b863ec96be076f3b7e9769e83bb2f902e7d44b54
JuanCasZa <juanjose-castro2@hotmail.com>
2025-11-24 22:44:59 -0500

    asp_presentaciones

---
+++ asp_presentaciones/Pages/Index.cshtml.cs
@@ -0,0 +1,135 @@
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace asp_presentaciones.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        // IMPLEMENTANDO COSAS
+        private readonly IUsuariosPresentacion? iPresentacion;    
+
+        public IndexModel(IUsuariosPresentacion presentacion)
+        {
+            iPresentacion = presentacion;
+        }
+        //
+
+        public bool EstaLogueado = false;
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public string? Email { get; set; }
+        [BindProperty] public string? Contrasenha { get; set; }
+
+        public void OnGet()
+        {
+            var variable_session = HttpContext.Session.GetString("Usuario");
+            if (!String.IsNullOrEmpty(variable_session))
+            {
+                EstaLogueado = true;
+                return;
+            }
+        }
+
+        public void OnPostBtClean()
+        {
+            try
+            {
+                Email = string.Empty;
+                Contrasenha = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public async Task OnPostBtEnter()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Email) &&
+                    string.IsNullOrEmpty(Contrasenha))
+                {
+                    OnPostBtClean();
+                    return;
+                }
+
+                // IMPLEMENTANDO COSAS
+                var Token = HttpContext.Session.GetString("Token"); //Implementando cosas                
+
+                var token = await iPresentacion!.Autenticar(Email!, Contrasenha!, Token!);
+
+                if (token == null)
+                {
+                    ViewData["Mensaje"] = "Usuario o contraseña incorrecta";
+                    OnPostBtClean();
+                    return;
+                }
+
+                //
+
+                //Usuario quemado, hay que cambiarlo para traer a los usuarios acá
+                /*
+                if ("admin.123" != Email + "." + Contrasenha)
+                {
+                    OnPostBtClean();
+                    return;
+                }*/
+
+                ViewData["Logged"] = true;
+                HttpContext.Session.SetString("Usuario", Email!);
+                HttpContext.Session.SetString("Token", token); //IMPLEMENTANDO COSAS
+                //var rol = await iPresentacion.ObtenerRol(token); //IMPLEMENTANDO COSAS
+                //ViewData["Rol"] = rol; //IMPLEMENTANDO COSAS
+                //HttpContext.Session.SetString("Rol", rol ?? ""); //IMPLEMENTANDO COSAS
+                EstaLogueado = true;
+                OnPostBtClean();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtClose()
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+                HttpContext.Response.Redirect("/");
+                EstaLogueado = false;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        //Para cerrar el cuadro emergente
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                {
+                    OnPostBtClean();
+                    HttpContext.Response.Redirect("/");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        
+        
+    }
+}
---
+++ asp_presentaciones/Pages/Ventanas/Usuarios.cshtml.cs
@@ -0,0 +1,174 @@
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class UsuariosModel : PageModel
+    {
+        //IMPORTANTE: MIRAR COMO HACER PARA QUE LOS BOTONES DE MODIFICAR, BORRAR Y EL LINK FUNCIONEN CORRECTAMENTE
+
+        private IUsuariosPresentacion? iPresentacion = null;
+
+        public UsuariosModel(IUsuariosPresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        //Se agrego para hacer el desplegable
+        public List<(int Id, string Nombre)> Roles { get; set; } = new()
+        {
+            (1, "Administrador"),
+            (2, "Entrenador"),
+            (3, "Recepcionista"),
+            (4, "Ventas")
+        };
+        //
+
+        public IFormFile? FormFile { get; set; }
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Usuarios? Actual { get; set; }
+        [BindProperty] public List<Usuarios>? Lista { get; set; }
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                var token = HttpContext.Session.GetString("Token"); //IMPLEMENTANDO COSAS
+                var variable_session = HttpContext.Session.GetString("Usuario");
+                if (String.IsNullOrEmpty(variable_session))
+                {
+                    HttpContext.Response.Redirect("/");
+                    return;
+                }
+
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Listar(token!);
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Usuarios();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                var token = HttpContext.Session.GetString("Token"); //Implementando cosas
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Usuarios>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.Guardar(Actual!)!;
+                else
+                    task = this.iPresentacion!.Modificar(Actual!, token! /*Implementando cosas*/)!;
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var token = HttpContext.Session.GetString("Token"); //Implementando cosas
+                var task = this.iPresentacion!.Borrar(Actual!, token!/*Implementando cosas*/);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                HttpContext.Response.Redirect("/"); //
+                //if (Accion == Enumerables.Ventanas.Listas)
+                //OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+    }
+}
